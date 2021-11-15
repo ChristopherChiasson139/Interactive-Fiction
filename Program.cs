03ba08e9995b7e5a7b511d92bc8025d86c0efc39
Christopher Chiasson <90638967+ChristopherChiasson139@users.noreply.github.com>
2021-11-15 17:36:06 -0400

    a start

--- Program.cs
+++ Program.cs
@@ -4,12 +4,62 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace template
+namespace string__
 {
     class Program
     {
+        static string[] page = new string[5];
+        static bool reset = true;
+
+
         static void Main(string[] args)
         {
+
+            while (reset == true)
+            {
+
+                page[0] = "eee";
+                page[1] = "aaa";
+                while (Console.KeyAvailable)
+                {
+                    Console.ReadKey(true);
+                }
+                ConsoleKeyInfo input;
+                input = Console.ReadKey(true);
+
+
+                if (input.KeyChar == 'e')
+                {
+                    //reset = false;
+                    Console.WriteLine(page[0]);
+                }
+                else
+                {
+                    Console.WriteLine("not an option");
+                    
+                }
+
+                
+
+                //--------------------------------------//
+                
+                
+                
+
+
+                if (input.KeyChar == 'a')
+                {
+                    //reset = false;
+                    Console.WriteLine(page[1]);
+                }
+
+                
+
+                //--------------------------------------//
+
+
+            }
+
         }
     }
 }
