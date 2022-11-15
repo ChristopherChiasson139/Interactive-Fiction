9cc4b061c67c585f98d1e3b5b9d46b8e1d5257f8
Christopher Chiasson <90638967+ChristopherChiasson139@users.noreply.github.com>
2022-11-15 09:52:36 -0400

    Initial commit

---
+++ Interactive Fiction/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interactive_Fiction
+{
+    internal class Program
+    {
+        static public string[,] story;
+        static bool gamerun;
+        static public int a = 1;
+        static public int b = 1;
+        static void Main(string[] args)
+        {
+            story = new string[10, 10];
+            story[1, 1] = "1";
+            story[1, 2] = "2";
+            story[1, 3] = "3";
+            story[1, 4] = "4";
+
+            gamerun = true;
+            while (gamerun)
+            {
+                ConsoleKeyInfo input;
+                input = Console.ReadKey(true);
+
+                switch (story[a, b])
+                {
+                    case "4":
+                        gamerun = false;
+                        break;
+                }
+
+                Console.WriteLine(story[a, b]);
+                if (input.KeyChar == 'a')
+                {
+                    a = a + 1;
+                }
+                if (input.KeyChar == 'b')
+                {
+                    b = b + 1;
+                }
+
+                
+            }
+
+
+            Console.WriteLine("did");
+            Console.ReadKey(true);
+        }
+        
+    }
+}
